8746b0130383594204fc4287227e7f08d04e261d
Emily M <mle.jo.11@gmail.com>
2019-07-24 17:43:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] ) # <== Unique work
+    new.push( source_array[i] * source_array[i] )
     i += 1
   end
   return new
