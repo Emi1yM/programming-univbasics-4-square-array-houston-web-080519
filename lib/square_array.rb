6c80d60cbd32faa1a7003b73f31671c500475bd1
Emily M <mle.jo.11@gmail.com>
2019-07-24 17:42:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,12 @@ def square_array(numbers)
   end
 end
 
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] ) # <== Unique work
+    i += 1
+  end
+  return new
+end
